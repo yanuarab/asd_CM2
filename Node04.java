0cb710da39cd1f1277298c862c8aada7d57aba48
JustinLaiya <laiyajustin@gmail.com>
2025-06-05 14:07:39 +0700

    Okehhh

---
+++ Node04.java
@@ -0,0 +1,9 @@
+public class Node04 {
+    Kendaraan04 data;
+    Node04 next;
+
+    public Node04(Kendaraan04 data) {
+        this.data = data;
+        this.next = null;
+    }
+}
---
+++ TransaksiPengisian04.java
@@ -0,0 +1,19 @@
+public class TransaksiPengisian04 {
+    Kendaraan04 kendaraan;
+    BBM04 bbm;
+    double liter, totalBayar;
+
+    public TransaksiPengisian04(Kendaraan04 Kendaraan, BBM04 bbm, double liter) {
+        this.kendaraan = kendaraan;
+        this.bbm = bbm;
+        this.liter = liter;
+        this.totalBayar = liter * bbm.hargaPerLiter;
+    }
+
+    public void tampilkanTransaksi() {
+        kendaraan.tampilkanInformasi();
+        System.out.println("BBM: " + bbm.namaBBM);
+        System.out.println("Liter: " + liter);
+        System.out.println("Total bayar: RP" + totalBayar);
+    }
+}
